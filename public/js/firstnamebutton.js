5b7c38c6c0127e59bf518cf5f83fdb47eadc19e7
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2023-02-23 21:28:29 -0800

    first name button running

---
+++ public/js/firstnamebutton.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const firstnameButton = document.getElementById("firstname-button");
+    firstnameButton.onclick = function() {
+      window.location.href = "/firstname";
+    }
+  });
--- public/js/homebutton.js
+++ public/js/homebutton.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const streetnameButton = document.getElementById("home-button");
-    streetnameButton.onclick = function() {
+    const homeButton = document.getElementById("home-button");
+    homeButton.onclick = function() {
       window.location.href = "/";
     }
   });
