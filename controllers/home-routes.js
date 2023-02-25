9a91a6c8961f80be945525f33d5052c3c6e0cf7e
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2023-02-25 15:36:32 -0800

    restructuring files. server was very crowded, made controllers/routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,153 @@
+const router = require('express').Router();
+const mysql = require('mysql2');
+const { engine } = require('express-handlebars');
+const consoleTable = require('console.table');
+
+
+
+
+//Create a connection to the mysql server
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Carmel360',
+    database: 'parcel_db'
+  });
+
+
+// Render the home page with the parcel data
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM mytable', (error, results, fields) => {
+      if (error) {
+        console.error('Error retrieving data from database: ' + error.stack);
+        res.status(500).send('Internal server error');
+        return;
+      }
+      // Add maplink column to the result
+      const parcels = results.map((parcel) => {
+        const address = parcel.address;
+        const api_key = 'AIzaSyBitp-D5fzf3sFeHVQ8idSV62EFjf6y5AM'
+        const maplink = `https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${address}, Mazama, WA`
+        return { ...parcel, maplink};
+      });
+      res.render('home', {parcels});
+    });
+  });
+
+  router.get('/map', (req, res) => {
+    const address = req.query.address;
+    const address_with_city_state = `${address}, Mazama, WA`;
+    const api_key = 'AIzaSyBitp-D5fzf3sFeHVQ8idSV62EFjf6y5AM'; 
+    const maplink = `https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${address_with_city_state}`
+    res.render('map', { maplink });
+  });
+
+  // Render table by street name 
+router.get('/streetname', function(req, res){
+    connection.query('SELECT * FROM mytable ORDER BY address', function(error, results, fields){
+        if (error) throw error;
+        var data = groupDataByStreetName(results);
+        res.render('streetname', { data });
+    });
+    
+  });
+  
+  function groupDataByStreetName(data) {
+    // Create an object to hold the grouped data
+    const groupedData = {};
+    // Check if data is an array
+    if (!Array.isArray(data)) {
+      return groupedData;
+    }
+    // Loop through each row of data
+    data.forEach(row => {
+      // Extract the street name from the address field
+      const streetName = row.address.split(' ')[1];
+  
+      // If this is the first row with this street name, create a new array
+      if (!groupedData[streetName]) {
+        groupedData[streetName] = [];
+      }
+  
+      // Add this row to the array for the street name
+      groupedData[streetName].push(row);
+    });
+  
+    // Sort the grouped data by street name
+    const sortedData = {};
+    Object.keys(groupedData).sort().forEach(streetName => {
+      sortedData[streetName] = groupedData[streetName];
+    });
+  
+    return sortedData;
+  }
+
+  // If we want to add street number, this renders not on the table, but can be changed to do so
+
+// app.get('/streetnumber', function(req, res){
+//   connection.query('SELECT * FROM mytable ORDER BY CAST(SUBSTRING_INDEX(address, " ", 1) AS UNSIGNED)', function(error, results, fields){
+//       if (error) throw error;
+//       var data = groupDataByStreetNumber(results);
+//       res.send(data);
+//   });
+// });
+
+// function groupDataByStreetNumber(data) {
+//   const groups = {};
+//   for (let i = 0; i < data.length; i++) {
+//     const address = data[i].address;
+//     if (!address) continue;
+//     const streetNumber = parseInt(address.split(' ')[0]);
+//     if (isNaN(streetNumber)) continue;
+//     if (!groups[streetNumber]) {
+//       groups[streetNumber] = [];
+//     }
+//     groups[streetNumber].push(data[i]);
+//   }
+//   const sortedGroups = {};
+//   Object.keys(groups).sort((a, b) => a - b).forEach(key => {
+//     sortedGroups[key] = groups[key];
+//   });
+//   return sortedGroups;
+// }
+
+// Render table to sort by first name 
+router.get('/firstname', function(req, res) {
+    connection.query('SELECT * FROM mytable ORDER BY owner', function(error, results, fields) {
+      if (error) throw error;
+      const data = groupDataByOwner(results);
+      res.render('firstname', {data: data}); // pass the data object to the view
+    });
+  });
+  
+  function groupDataByOwner(results) {
+    const data = {};
+    for (let i = 0; i < results.length; i++) {
+      const row = results[i];
+      if (!row.owner) { // check if owner field is null or undefined
+        continue; // skip this row
+      }
+      const commaIndex = row.owner.indexOf(",");
+      if (commaIndex === -1) { // check if comma is not found in owner field
+        continue; 
+      }
+      const firstName = row.owner.substring(commaIndex + 1).trim();
+      if (!firstName) {
+        continue; 
+      }
+      if (!data[firstName]) {
+        data[firstName] = [];
+      }
+      data[firstName].push(row);
+    }
+    // sort the keys in alphabetical order
+    const sortedKeys = Object.keys(data).sort();
+    const sortedData = {};
+    for (let i = 0; i < sortedKeys.length; i++) {
+      const key = sortedKeys[i];
+      sortedData[key] = data[key];
+    }
+    return sortedData;
+  }
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,186 +1,20 @@
 const express = require('express');
-const mysql = require('mysql2');
+const routes = require('./controllers/home-routes');
 const { engine } = require('express-handlebars');
 
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.use('/', routes);
 
-
-//Create a connection to the mysql server
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Carmel360',
-    database: 'parcel_db'
-});
 
 // Set up Handlebars as the template engine
 app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));
 app.set('view engine', 'hbs');
 
-
-
-
-
-
-// Render the home page with the parcel data
-app.get('/', (req, res) => {
-  connection.query('SELECT * FROM mytable', (error, results, fields) => {
-    if (error) {
-      console.error('Error retrieving data from database: ' + error.stack);
-      res.status(500).send('Internal server error');
-      return;
-  }
-
-
- // Add maplink column to the result
-    const parcels = results.map((parcel) => {
-      const address = parcel.address;
-      const api_key = 'AIzaSyBitp-D5fzf3sFeHVQ8idSV62EFjf6y5AM'
-      const maplink = `https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${address}, Mazama, WA`
-      return { ...parcel, maplink};
-    });
-    res.render('home', {parcels});
-  });
-});
-
-app.get('/map', (req, res) => {
-  const address = req.query.address;
-  const address_with_city_state = `${address}, Mazama, WA`;
-  const api_key = 'AIzaSyBitp-D5fzf3sFeHVQ8idSV62EFjf6y5AM'; 
-  const maplink = `https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${address_with_city_state}`
-  res.render('map', { maplink });
-});
-
-
-
-
-
-// Render table by street name 
-
-app.get('/streetname', function(req, res){
-  connection.query('SELECT * FROM mytable ORDER BY address', function(error, results, fields){
-      if (error) throw error;
-      var data = groupDataByStreetName(results);
-      res.render('streetname', { data });
-  });
-  
-});
-
-function groupDataByStreetName(data) {
-  // Create an object to hold the grouped data
-  const groupedData = {};
-
-  // Check if data is an array
-  if (!Array.isArray(data)) {
-    return groupedData;
-  }
-
-
-  // Loop through each row of data
-  data.forEach(row => {
-    // Extract the street name from the address field
-    const streetName = row.address.split(' ')[1];
-
-    // If this is the first row with this street name, create a new array
-    if (!groupedData[streetName]) {
-      groupedData[streetName] = [];
-    }
-
-    // Add this row to the array for the street name
-    groupedData[streetName].push(row);
-  });
-
-  // Sort the grouped data by street name
-  const sortedData = {};
-  Object.keys(groupedData).sort().forEach(streetName => {
-    sortedData[streetName] = groupedData[streetName];
-  });
-
-  return sortedData;
-}
-
-// If we want to add street number, this renders not on the table, but can be changed to do so
-
-// app.get('/streetnumber', function(req, res){
-//   connection.query('SELECT * FROM mytable ORDER BY CAST(SUBSTRING_INDEX(address, " ", 1) AS UNSIGNED)', function(error, results, fields){
-//       if (error) throw error;
-//       var data = groupDataByStreetNumber(results);
-//       res.send(data);
-//   });
-// });
-
-// function groupDataByStreetNumber(data) {
-//   const groups = {};
-//   for (let i = 0; i < data.length; i++) {
-//     const address = data[i].address;
-//     if (!address) continue;
-//     const streetNumber = parseInt(address.split(' ')[0]);
-//     if (isNaN(streetNumber)) continue;
-//     if (!groups[streetNumber]) {
-//       groups[streetNumber] = [];
-//     }
-//     groups[streetNumber].push(data[i]);
-//   }
-//   const sortedGroups = {};
-//   Object.keys(groups).sort((a, b) => a - b).forEach(key => {
-//     sortedGroups[key] = groups[key];
-//   });
-//   return sortedGroups;
-// }
-
-
-// Render table to sort by first name 
-app.get('/firstname', function(req, res) {
-  connection.query('SELECT * FROM mytable ORDER BY owner', function(error, results, fields) {
-    if (error) throw error;
-    const data = groupDataByOwner(results);
-    res.render('firstname', {data: data}); // pass the data object to the view
-  });
-});
-
-function groupDataByOwner(results) {
-  const data = {};
-  for (let i = 0; i < results.length; i++) {
-    const row = results[i];
-    if (!row.owner) { // check if owner field is null or undefined
-      continue; // skip this row
-    }
-    const commaIndex = row.owner.indexOf(",");
-    if (commaIndex === -1) { // check if comma is not found in owner field
-      continue; 
-    }
-    const firstName = row.owner.substring(commaIndex + 1).trim();
-    if (!firstName) {
-      continue; 
-    }
-    if (!data[firstName]) {
-      data[firstName] = [];
-    }
-    data[firstName].push(row);
-  }
-  // sort the keys in alphabetical order
-  const sortedKeys = Object.keys(data).sort();
-  const sortedData = {};
-  for (let i = 0; i < sortedKeys.length; i++) {
-    const key = sortedKeys[i];
-    sortedData[key] = data[key];
-  }
-  return sortedData;
-}
-
-
-
-// Start the server
-
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
-
-
-
-
